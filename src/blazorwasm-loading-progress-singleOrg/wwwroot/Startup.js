5ce917e737124df9795fb841b2e2d7f25657592b
MareMare <MareMare@users.noreply.github.com>
2022-10-07 15:49:12 +0900

    Startup.js に切り出します。

---
+++ src/blazorwasm-loading-progress-singleOrg/wwwroot/Startup.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+    var loadedCount = 0;
+    var allResourcesBeingLoaded = [];
+    Blazor.start({ // start manually with loadBootResource
+        loadBootResource: function (type, filename, defaultUri, integrity) {
+            console.log(`Loading: '${type}', '${filename}', '${defaultUri}'`);
+
+            if (type == "dotnetjs")
+                return defaultUri;
+
+            var fetchResources = fetch(defaultUri,
+                {
+                    cache: 'no-cache',
+                    integrity: integrity,
+                    headers: { 'MyCustomHeader': 'My custom value' }
+                });
+
+
+            allResourcesBeingLoaded.push(fetchResources);
+            fetchResources.then((r) => {
+                loadedCount++;
+
+                var total = allResourcesBeingLoaded.length;
+                var progressbar = document.getElementById('progressbar');
+                var value = parseInt((loadedCount * 100.0) / total);
+                var pct = value + '%';
+                progressbar.style.width = pct;
+                console.info(`${loadedCount}/${total} (${pct}): (${type}) ${filename}`);
+
+                var progressLabel = document.getElementById('progressLabel');
+                progressLabel.innerText = `$Downloading ${loadedCount}/${total}: (${type}) ${filename}`;
+            });
+            return fetchResources;
+        }
+    });
+});
